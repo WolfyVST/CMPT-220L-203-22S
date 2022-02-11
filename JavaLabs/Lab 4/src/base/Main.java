1abad6c6be525e8daaed6d9cbf66557ba1115e02
Ricardo Villalobos <60368728+WolfyVST@users.noreply.github.com>
2022-02-11 16:12:21 -0500

    Finised lab 4

--- JavaLabs/Lab 4/src/base/Main.java
+++ JavaLabs/Lab 4/src/base/Main.java
@@ -1,5 +1,5 @@
 package base;
-
+import static java.lang.Math.*;
 public class Main {
     /**
      * # Lab Four
@@ -16,8 +16,43 @@ public class Main {
      */
     public static void main(String[] args) {
         // Give me an example of you using switch case.
+        String vegetales = "potato";
+        switch (vegetales){
+            case "lettuce":
+                System.out.println("salad");
+                break;
+            case "tomato":
+                System.out.println("ketchup");
+                break;
+            case "onion":
+                System.out.println("Onion Rings");
+                break;
+            case "potato":
+                System.out.println("fries");
+                break;
+
+        }
         // Give me an example of you using a for loop
+        for (int i = 1; i < 13; i++){
+            System.out.println(2 * i);
+        }
         // Give me an example of you using a while loop
+
+        int counter = 0 ;
+        while (counter <= 10){
+            System.out.println("Counting number " + counter);
+            counter++;
+        }
+        //first function
+        int resutl = powerednumber(5,3);
+        System.out.println(resutl);
+        //second function
+        int[] y = new int[]{1,2,4,2,5,2};
+        System.out.println(listreader(2, y ));
+        //third fuction
+        System.out.println(sumarr(y));
+        //fourth function
+        System.out.println(pytheory(3,4));
     }
     // For the following todos you'll probably want to define them below and then call them from the main function above.
 
@@ -26,19 +61,37 @@ public class Main {
      * For example if given the numbers 2 and 3. The math that the function should do is 2^3
      * and should print out or return 8. Print the output.
      */
-
+   static int powerednumber (int num1 , int num2) {
+        return num1^num2;
+    }
     /**
      * I want you to write a function that will take in a list and see how many times a given number is in the list.
      * For example if the array given is [2,3,5,2,3,6,7,8,2] and the number given is 2 the function should print out
      * or return 3. Print the output.
      */
+   static int listreader (int num, int[] list ){
 
+       int x = 0;
+       for (int i = 0; i < list.length; i++){
+           if (list[i] == num){
+               x++;
+           }
+       }
+
+       return x;
+   }
     /**
      * Create a function named summation that will take in an int[ ] and return an int.
      * I want the function to then go through the given array and sum up digits in the array.
      * Once that’s done print or return the answer. Print the output
      */
-
+    static int sumarr (int[] list2 ){
+        int z = 0 ;
+        for (int i = 0; i <list2.length; i++){
+            z =  z + list2[i];
+        }
+        return z;
+    }
     /**
      * Give me a function that gives the answer to the pythagorean theorem.
      * I'd like you to reuse the exponent function from above as well as the functions below to make your function.
@@ -51,13 +104,9 @@ public class Main {
      * Once that’s done print or return the answer. Print the output
      */
 
-    int addition(int a, int b) {
-        int answer = a + b;
-        return answer;
-    }
 
-    int division(int a, int b) {
-        int answer = a / b;
-        return answer;
+    static double pytheory (int a , int b){
+        double c = Math.sqrt(powerednumber(a,2) + powerednumber(b,2));
+        return c;
     }
 }
